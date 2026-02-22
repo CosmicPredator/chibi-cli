fc933a147df12c78638f6a1c767a1334197bf9ee
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-22 12:41:26 +0530

    update constants for issue #24
    all my following commits will be for issue #24

--- internal/constants.go
+++ internal/constants.go
@@ -5,9 +5,12 @@ package internal
 // Base URL is not gonna get changed for a while.
 // So, keeping it as constant is not gonna hurt anyone.
 const (
-	API_ENDPOINT     = "https://graphql.anilist.co"
-	AUTH_URL         = "https://anilist.co/api/v2/oauth/authorize?client_id=4593&response_type=token"
-	DB_PATH          = "chibi_config.db"
+	API_ENDPOINT    = "https://graphql.anilist.co"
+	AUTH_URL        = "https://anilist.co/api/v2/oauth/authorize?client_id=4593&response_type=token"
+	APP_DIR_NAME    = "chibi"
+	DB_PATH         = "chibi_config.db"
+	DATA_PATH_ENV   = "CHIBI_DATA_PATH"
+	LEGACY_PATH_ENV = "CHIBI_PATH"
 )
 
 type MediaType string
